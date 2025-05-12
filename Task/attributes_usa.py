f4c394d20f57bdbef42f428db35de347f51b78e4
mvdk <martvdkam@gmail.com>
2025-05-12 13:34:56 +0200

    Add files via upload

--- Task/attributes_usa.py
+++ Task/attributes_usa.py
@@ -992,112 +992,114 @@ attributes_policy_mix_support = [
     {
         'Subsidy on investment costs <b>new</b> EVs': '$0',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$0',
-        'Carbon tax': '$0 per 100 miles',
+        'CO2 tax': '$0 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2030'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$5,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$5,000',
-        'Carbon tax': '$0 per 100 miles',
+        'CO2 tax': '$0 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2030'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$5,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$2,000',
-        'Carbon tax': '$6 per 100 miles',
+        'CO2 tax': '$6 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2030'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$2,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$5,000',
-        'Carbon tax': '$6 per 100 miles',
+        'CO2 tax': '$6 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2030'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$2,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$0',
-        'Carbon tax': '$12 per 100 miles',
+        'CO2 tax': '$12 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2030'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$0',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$2,000',
-        'Carbon tax': '$12 per 100 miles',
+        'CO2 tax': '$12 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2030'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$5,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$2,000',
-        'Carbon tax': '$0 per 100 miles',
+        'CO2 tax': '$0 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2040'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$2,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$5,000',
-        'Carbon tax': '$0 per 100 miles',
+        'CO2 tax': '$0 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2040'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$0',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$0',
-        'Carbon tax': '$6 per 100 miles',
+        'CO2 tax': '$6 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2040'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$2,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$2,000',
-        'Carbon tax': '$6 per 100 miles',
+        'CO2 tax': '$6 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2040'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$5,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$0',
-        'Carbon tax': '$12 per 100 miles',
+        'CO2 tax': '$12 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2040'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$0',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$5,000',
-        'Carbon tax': '$12 per 100 miles',
+        'CO2 tax': '$12 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': '2040'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$2,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$0',
-        'Carbon tax': '$0 per 100 miles',
+        'CO2 tax': '$0 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': 'Never'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$0',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$2,000',
-        'Carbon tax': '$0 per 100 miles',
+        'CO2 tax': '$0 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': 'Never'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$5,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$0',
-        'Carbon tax': '$6 per 100 miles',
+        'CO2 tax': '$6 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': 'Never'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$0',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$5,000',
-        'Carbon tax': '$6 per 100 miles',
+        'CO2 tax': '$6 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': 'Never'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$2,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$2,000',
-        'Carbon tax': '$12 per 100 miles',
+        'CO2 tax': '$12 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': 'Never'
     },
     {
         'Subsidy on investment costs <b>new</b> EVs': '$5,000',
         'Subsidy on investment costs <b>pre-owned</b> EVs': '$5,000',
-        'Carbon tax': '$12 per 100 miles',
+        'CO2 tax': '$12 per 100 miles',
         'Sales ban on gasoline and diesel vehicles from': 'Never'
     },
 ]
+
+
 
 attributes_policy_outcome_fairness = [
     {
