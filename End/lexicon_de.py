d5c2971448efb8509e31b07176cd0b7b4f729d0d
mvdk <martvdkam@gmail.com>
2025-07-28 15:32:19 +0200

    Delete End directory

--- End/lexicon_de.py
+++
@@ -1,5 +0,0 @@
-class Lexicon:
-    end_title = 'Ende'
-    thanks = "Vielen Dank für Ihre Teilnahme."
-    redirect_instructions = "BITTE KLICKEN SIE UNTEN, UM DIE BEFRAGUNG  ABZUSCHLIESSEN UND IHRE ERGEBNISSE EINZUREICHEN"
-    finish = "Hier klicken, um zu beenden"
